646e4d692c10d5ecb0d1b094506812ac4dfa43ee
ybkimdev <88050450+ybkimdev@users.noreply.github.com>
2025-07-07 11:23:35 +0900

    feat: 그룹 분석 기능#156  (#158)
    * feat:Add BE API Logic
    * feat:Add Model Version
    * Add new Packages in requirements.txt

--- src/api/controllers/analysis_controller.py
+++ src/api/controllers/analysis_controller.py
@@ -1,29 +1,43 @@
 import json
 from fastapi import APIRouter, BackgroundTasks, status, Request
+import pytz
 from api.dtos.analysis_request import AnalysisRequest
 from services.analysis_pipeline import run_analysis_pipeline
+from datetime import datetime, timedelta
 
 def get_word_router(moderation_task_queue, result_queue):
     router = APIRouter(prefix="/api/v1", tags=["Analysis"])
 
     @router.post("/analysis")
-    async def create_word(request: Request, body: AnalysisRequest, background_tasks: BackgroundTasks):
+    async def analyze(request: Request, background_tasks: BackgroundTasks):
+        kst = pytz.timezone("Asia/Seoul")
+        now_kst = datetime.now(kst)
+        today_weekday = now_kst.weekday()
+        this_monday_kst = (now_kst - timedelta(days=today_weekday)).replace(hour=0, minute=0, second=0, microsecond=0)
+        last_monday_kst = this_monday_kst - timedelta(days=7)
+        last_sunday_kst = this_monday_kst - timedelta(days=1)
+
+        last_monday_utc = last_monday_kst.astimezone(pytz.utc)
+        last_sunday_utc = last_sunday_kst.astimezone(pytz.utc)
+
+        # ISO8601 포맷 문자열 반환 (밀리초/타임존 제외)
+        start_date = last_monday_utc.strftime("%Y-%m-%dT%H:%M:%S")
+        end_date = last_sunday_utc.strftime("%Y-%m-%dT%H:%M:%S")
 
         # logger 가져오기
         logger = request.app.state.logger
-        # moderation.log에 기록
         logger.info(
             json.dumps({
                 "method": "analysis",
-                "start_date": body.start_date,
-                "end_date": body.end_date,
+                "start_date": start_date,
+                "end_date": end_date,
                 "client_host": request.client.host
             }),
             extra={"section": "analysis"}
         )
 
         # Queue에 데이터 삽입
-        background_tasks.add_task(run_analysis_pipeline, body.start_date, body.end_date, moderation_task_queue, result_queue, logger)
+        background_tasks.add_task(run_analysis_pipeline, start_date, end_date, moderation_task_queue, result_queue, logger)
         return {"method": "alanysis", "status": "queued"}
 
     return router 
